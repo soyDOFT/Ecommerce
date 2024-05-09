6f134443254e85ca039dd7c7fbee616efe40f7d8
David Flores <soydoft@gmail.com>
2024-05-09 18:34:46 -0400

    Add product json and improve footer

---
+++ contactform.js
@@ -0,0 +1,21 @@
+function validateContactForm() {
+    e.preventDefault();
+
+    let finalAlert = "Error: ";
+    let fname = document.forms["contact-form"]["fname"].value;
+    let regex = /^[0-9]{1,50}$/;
+    if (fname === "" || regex.test(fname)) {
+        finalAlert += "Enter your valid first name.\n";
+    }
+
+    let lname = document.forms["contact-form"]["lname"].value;
+    if (lname === "" || regex.test(lname)) {
+        finalAlert += "Enter your valid last name.\n";
+    }
+
+    if (finalAlert === "Error: ") {
+        window.location.assign("thankyou.html");
+    } else {
+        alert(finalAlert);
+    }
+}
